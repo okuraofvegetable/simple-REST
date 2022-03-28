e4b4c3786b4d99b9e5ea8e502c3469039f59c838
shinji <okuraofvegetable@gmail.com>
2022-03-28 14:37:09 +0900

    modify status code

--- index.js
+++ index.js
@@ -34,20 +34,20 @@ app.post('/signup', (req, res) => {
 
   if (user.user_id === void 0 || user.password === void 0) {
     res_fail.cause = 'required';
-    return res.status(404).send(res_fail);
+    return res.status(400).send(res_fail);
   }
 
 
   if (user.user_id.length < 6 || user.user_id.length > 20 || user.password.length < 8 || user.password.length > 20) {
     res_fail.cause = 'length';
-    return res.status(404).send(res_fail);
+    return res.status(400).send(res_fail);
   }
 
   const regex = /^[a-zA-Z0-9]*$/;
   if (!user.user_id.match(regex)) {
     res_fail.cause = 'pattern';
     console.log(user.user_id);
-    return res.status(404).send(res_fail);
+    return res.status(400).send(res_fail);
   }
 
   for (var i = 0; i < user.password.length; i++) {
@@ -55,7 +55,7 @@ app.post('/signup', (req, res) => {
     if (code < 33 || code == 127 || isNaN(code)) {
       res_fail.cause = 'pattern';
       console.log(code);
-      return res.status(404).send(res_fail);
+      return res.status(400).send(res_fail);
     }
   }
 
